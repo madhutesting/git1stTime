90a2b7c399531f2c5e402c8c3eab916ac2269c2d
madhutesting <kissuv.0912@gmail.com>
2022-06-28 14:43:40 +0530

    madhus commit closed test cases

---
+++ src/main/java/V_MADHU_K/maventest2/App.java
@@ -0,0 +1,13 @@
+package V_MADHU_K.maventest2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/V_MADHU_K/maventest2/LoginTest.java
@@ -0,0 +1,33 @@
+package V_MADHU_K.maventest2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+		@Test
+		public void login() {
+			WebDriverManager.chromedriver().setup();
+			WebDriver dr = new ChromeDriver();
+			dr.manage().window().maximize();
+			
+			
+			dr.get("https://opensource-demo.orangehrmlive.com/");
+			
+			dr.findElement(By.id("txtUsername")).sendKeys("Admin");
+			dr.findElement(By.id("txtPassword")).sendKeys("Admin123");
+			dr.findElement(By.id("btnLogin")).click();
+			
+			Assert.assertEquals(dr.getTitle(), "OrangeHRM");
+			
+			dr.close();
+		}
+
+	}
+
+
+
