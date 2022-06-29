60acf75ca03bd24563e1c23417a1a385a8aadbdd
madhutesting <kissuv.0912@gmail.com>
2022-06-29 12:11:10 +0530

    person1 changes

--- src/test/java/V_MADHU_K/maventest2/NewTest.java
+++ src/test/java/V_MADHU_K/maventest2/NewTest.java
@@ -3,6 +3,8 @@ package V_MADHU_K.maventest2;
 public class NewTest {
 	public static void main(String[] args) {
 		System.out.println("kishore");
+		System.out.println("person1 changes");
+		System.out.println("person1 changes");
 	}
 
 }
